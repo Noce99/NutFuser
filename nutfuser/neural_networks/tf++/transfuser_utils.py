ae92484f936cd4b46f24807768836e635427a03c
Noce99 <enrico.mannocci99@gmail.com>
2024-04-05 14:54:26 +0200

    Added server carla location!

--- nutfuser/neural_networks/tf++/transfuser_utils.py
+++ nutfuser/neural_networks/tf++/transfuser_utils.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append("/leonardo_work/IscrC_SSNeRF/CARLA_0.9.15/PythonAPI/carla/dist/carla-0.9.15-py3.7-linux-x86_64.egg")
 sys.path.append("/home/enrico/Progetti/Carla/PythonAPI/carla/dist/carla-0.9.15-py3.7-linux-x86_64.egg")
 sys.path.append("/home/enrico/Projects/Carla/CARLA_0.9.15/PythonAPI/carla/dist/carla-0.9.15-py3.7-linux-x86_64.egg")
+sys.path.append("/leonardo_scratch/fast/IscrC_SSNeRF/CARLA_0_9_15/PythonAPI/carla/dist/carla-0.9.15-py3.7-linux-x86_64.egg")
 import carla
 import numpy as np
 import torch
