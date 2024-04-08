d60ee07fae69aea75a70c23bed17409eb1d20808
Noce99 <enrico.mannocci99@gmail.com>
2024-04-08 19:09:55 +0200

    Added the possibility to use pretrained weight!

--- train_tf++_with_its_code.py
+++ train_tf++_with_its_code.py
@@ -34,7 +34,7 @@ def get_arguments():
         required=False,
         default=None,
         type=str
-    )type=str
+    )
     argparser.add_argument(
         '--just_backbone',
         help='Set if you want to train just the backbone!',
