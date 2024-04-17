7e0c05cc732db98f5547a42437b7eac257139cb5
Noce99 <enrico.mannocci99@gmail.com>
2024-04-17 10:45:01 +0200

    Improved generate_scripts.py, now is more user friendly!

--- generate_scripts.py
+++ generate_scripts.py
@@ -9,31 +9,33 @@ from nutfuser import utils
 
 Job = collections.namedtuple("Job", ["id", "town", "port", "tm_port"])
 
-
 if __name__ == "__main__":
 
-    # CREATING scripts_and_jobs FOLDER
-    if len(sys.argv) == 1:
-        print(utils.color_error_string("I need an argument with the dataset path!"))
-        exit()
-    config.DATASET_PATH = sys.argv[1]
-    if not os.path.isdir(config.DATASET_PATH):
+    # ASK where to put the scripts_and_jobs FOLDER
+    done = False
+    while not done:
         try:
-            os.mkdir(config.DATASET_PATH)
+            possible_path = input("Where to save the scripts_and_jobs FOLDER? : ")
+            if not os.path.isdir(possible_path):
+                print(utils.color_error_string(f"Cannot find '{possible_path}'!"))
+                continue
+            script_and_jobs_path = os.path.join(possible_path, "scripts_and_jobs")
+            if os.path.isdir(script_and_jobs_path):
+                print(utils.color_error_string(f"'scripts_and_jobs' folder already present in '{possible_path}'!"))
+                overwrite = None
+                while overwrite not in ["yes", "no"]:
+                    overwrite = input("Do you want to overwrite it? ['yes' or 'no'] : ")
+                if overwrite == "yes":
+                    shutil.rmtree(script_and_jobs_path)
+                else:
+                    raise KeyboardInterrupt
+            done = True
+        except KeyboardInterrupt:
+            print(utils.color_info_string("\nBye!\n"))
+            exit()
         except:
-            print(utils.color_error_string(f"Something bad with this path: '{config.DATASET_PATH}'"))
-            exit()
-    if os.path.isdir(os.path.join(config.DATASET_PATH, "scripts_and_jobs")):
-        print(utils.color_error_string(f"'scripts_and_jobs' folder already present in '{os.path.join(config.DATASET_PATH, 'scripts_and_jobs')}'!"))
-        overwrite = None
-        while overwrite not in ["yes", "no"]:
-            overwrite = input("Do you want to overwrite it? ['yes' or 'no'] : ")
-        if overwrite == "yes":
-            shutil.rmtree(os.path.join(config.DATASET_PATH, "scripts_and_jobs"))
-        else:
-            print("Bye!")
-            exit()
-    os.mkdir(os.path.join(config.DATASET_PATH, "scripts_and_jobs"))
+            continue
+    os.mkdir(script_and_jobs_path)
 
     # ASKING NUMBER OF JOBS TO CREATE
     done = False
@@ -41,6 +43,9 @@ if __name__ == "__main__":
         try:
             num_of_jobs = int(input("How many jobs do you want to run? [int number] : "))
             done = True
+        except KeyboardInterrupt:
+            print(utils.color_info_string("Bye!"))
+            exit()
         except:
             done = False
     print(utils.color_info_string(f"I will create {num_of_jobs} job files!"))
@@ -61,6 +66,30 @@ if __name__ == "__main__":
             done = False
     print(utils.color_info_string(f"Found Carla in {CARLA_PATH}!"))
 
+    # ASK NUMBER OF FRAMES
+    done = False
+    while not done:
+        try:
+            num_of_frames = int(input("How many frames per directory? (5000 is in [1; 2] GB per folder) [int number] : "))
+            done = True
+        except KeyboardInterrupt:
+            print(utils.color_info_string("Bye!"))
+            exit()
+        except:
+            done = False
+    
+    # ASK NUMBER OF OTHER NPC
+    done = False
+    while not done:
+        try:
+            num_of_npc = int(input("How many npc? (both cars and pedestrian) [int number] : "))
+            done = True
+        except KeyboardInterrupt:
+            print(utils.color_info_string("Bye!"))
+            exit()
+        except:
+            done = False
+
     # GET NUTFUSER FOLDER
     NUTFUSER = os.path.dirname(os.path.realpath(__file__))
 
@@ -72,16 +101,16 @@ if __name__ == "__main__":
     terminal 3: python3 spawn_npc.py --port 4000 --tm-port 4050 # TM-Server A connected to simulation A
     terminal 4: python3 spawn_npc.py --port 5000 --tm-port 5050 # TM-Server B connected to simulation B
     """
-    os.mkdir(os.path.join(config.DATASET_PATH, "scripts_and_jobs", "jobs"))
-    os.mkdir(os.path.join(config.DATASET_PATH, "scripts_and_jobs", "logs"))
+    os.mkdir(os.path.join(script_and_jobs_path, "jobs"))
+    os.mkdir(os.path.join(script_and_jobs_path, "logs"))
     for job in JOBS:
-        with open(os.path.join(config.DATASET_PATH, "scripts_and_jobs", "jobs", f"job_{job.id}.sh"), "w") as file:
+        with open(os.path.join(script_and_jobs_path, "jobs", f"job_{job.id}.sh"), "w") as file:
             file.write(
 f"""#!/bin/sh
 #SBATCH --job-name=j_{job.id}
 #SBATCH --partition=boost_usr_prod
-#SBATCH -o {os.path.join(config.DATASET_PATH, "scripts_and_jobs", "logs", f"{job.id}.log")}
-#SBATCH -e {os.path.join(config.DATASET_PATH, "scripts_and_jobs", "logs", f"{job.id}.log")}
+#SBATCH -o {os.path.join(script_and_jobs_path, "logs", f"{job.id}.log")}
+#SBATCH -e {os.path.join(script_and_jobs_path, "logs", f"{job.id}.log")}
 #SBATCH --mail-type=FAIL,BEGIN
 #SBATCH --mail-user=enrico.mannocci3@unibo.it
 #SBATCH --nodes=1
@@ -101,14 +130,14 @@ echo "Job on Town $TOWN started: $dt"
 
 cd {NUTFUSER}
 source bin/activate
-python generate_data.py --carla_path {CARLA_PATH} --town $TOWN --rpc_port $PORT --tm_port $TM_PORT --job_id $JOB_ID --dataset_path {config.DATASET_PATH} --num_of_frames 5000
+python generate_data.py --carla_path {CARLA_PATH} --town $TOWN --rpc_port $PORT --tm_port $TM_PORT --job_id $JOB_ID --dataset_path {config.DATASET_PATH} --num_of_frames {num_of_frames} --num_of_vehicle {num_of_npc} --num_of_walkers {num_of_npc}
 """         )
 
-    with open(os.path.join(config.DATASET_PATH, "scripts_and_jobs", "launch_all_jobs.sh"), "w") as file:
+    with open(os.path.join(script_and_jobs_path, "launch_all_jobs.sh"), "w") as file:
         file.write(
 f"""#!/bin/sh
 
-cd {os.path.join(config.DATASET_PATH, "scripts_and_jobs", "jobs")}
+cd {os.path.join(script_and_jobs_path, "jobs")}
 
 """     )
         for job in JOBS:
