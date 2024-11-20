b3c07e03b22d011f719c34f8d0f6c7ea4e756041
Noce99 <enrico.mannocci99@gmail.com>
2024-11-20 12:54:45 +0100

    Solved a bug in evaluation script generatioN!

--- nutfuser/neural_networks/tfpp/transfuser_utils.py
+++ nutfuser/neural_networks/tfpp/transfuser_utils.py
@@ -11,7 +11,10 @@ sys.path.append("/home/enrico/Projects/Carla/CARLA_0.9.15/PythonAPI/carla/dist/c
 sys.path.append("/leonardo_scratch/fast/IscrC_SSNeRF/CARLA_0_9_15/PythonAPI/carla/dist/carla-0.9.15-py3.7-linux-x86_64.egg")
 sys.path.append("/home/enrico/Projects/Carla/NutFuser/nutfuser/neural_networks/tfpp/")
 sys.path.append("/leonardo_scratch/fast/IscrC_ADC/NutFuser/nutfuser/neural_networks/tfpp/")
-import carla
+try:
+    import carla
+except ImportError:
+    print("Was not possible to import carla!")
 import numpy as np
 import torch
 import torch.nn.functional as F
